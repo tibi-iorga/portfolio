000e3d81cb060f2b18f534b0ed8ee34457d4cf0b
tibi <tibi@Hugos-iMac.local>
2019-05-15 16:13:08 +0100

    1

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
-
-<script type="text/javascript">
-  $(document).ready(function(){
-    $('.your-class').slick({
-      setting-name: setting-value
-    });
-  });
-</script>
+$('.your-class').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
